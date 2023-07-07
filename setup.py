bdeb093768f6166c73d7182f6736a081d8595714
moon <shawmakesmagic@gmail.com>
2023-07-06 17:07:41 -0700

    Updated to 0.1.7 and published

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
+long_description = ""
 with open("README.md", "r") as fh:
     long_description = fh.read()
+    # search for any lines that contain <img and remove them
+    long_description = long_description.split('\n')
+    long_description = [line for line in long_description if not '<img' in line]
+    # now join all the lines back together
+    long_description = '\n'.join(long_description)
+    
 
 setup(
     name='agentmemory',
-    version='0.1.6',
+    version='0.1.7',
     description='Easy-to-use agent memory, powered by chromadb',
     long_description=long_description,  # added this line
     long_description_content_type="text/markdown",  # and this line
