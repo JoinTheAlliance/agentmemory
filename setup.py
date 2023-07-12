e0105d29809332c62b7e843b33925a39a85b62df
moon <shawmakesmagic@gmail.com>
2023-07-12 13:26:08 -0700

    Updated to 0.1.12 and published

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='agentmemory',
-    version='0.1.11',
+    version='0.1.12',
     description='Easy-to-use agent memory, powered by chromadb',
     long_description=long_description,  # added this line
     long_description_content_type="text/markdown",  # and this line
