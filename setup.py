9f14718ea5d8f02b03707cdb3898a856832ebbbf
moon <shawmakesmagic@gmail.com>
2023-07-18 02:48:40 -0700

    remove version from setup

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ with open("README.md", "r") as fh:
 
 setup(
     name='agentmemory',
-    version='0.2.3',
     description='Easy-to-use agent memory, powered by chromadb',
     long_description=long_description,  # added this line
     long_description_content_type="text/markdown",  # and this line
